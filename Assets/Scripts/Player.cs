da459d4d29acc30de5d20198c6cf63185e3efd9d
Rob Drury <robjdrewry@gmail.com>
2023-07-21 15:22:19 +0200

    replaces player debugFloorSprite with gizmo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,15 @@ public class KinematicGravity : MonoBehaviour
     [SerializeField] private float gravityStrength = 10f;
     [SerializeField] private LayerMask groundLayer;
 
-    private Transform t;
     private bool isGrounded;
-    private float playerHeight;
-
-    private Transform debugFloorSprite;
+    private float playerHeight, playerWidth;
+    private Vector3 raycastStartingPos;
 
     private void Start()
     {
-        t = transform;
         playerHeight = GetComponent<CapsuleCollider2D>().bounds.size.y;
-        debugFloorSprite = t.Find("debugGo").transform;
+        playerWidth = GetComponent<CapsuleCollider2D>().bounds.size.x;
+        //Gizmos.color = Color.blue;
     }
 
     private void Update()
@@ -37,22 +35,30 @@ public class KinematicGravity : MonoBehaviour
         Vector2 gravityDirection = Vector2.down;
 
         // Apply the gravity force to the rigidbody.
-        t.position = new Vector3(t.position.x, t.position.y - gravityStrength * dt, t.position.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y - gravityStrength * dt, transform.position.z);
     }
 
     private bool CheckGrounded()
     {
-        Vector3 raycastStartingPos = new Vector3(t.position.x, t.position.y - (playerHeight / 2), t.position.z);
+        raycastStartingPos = new Vector3(transform.position.x, transform.position.y - (playerHeight / 2), transform.position.z);
 
         // Define a small raycast distance to check if the object is grounded.
         float raycastDistance = 0.1f;
 
-        debugFloorSprite.position = raycastStartingPos;
-        Debug.Log("ds [" + debugFloorSprite.transform.position + "] | rcs [" + raycastStartingPos + "]");
         // Cast a ray directly down to check if the object is touching the ground.
         RaycastHit2D hit = Physics2D.Raycast(raycastStartingPos, Vector2.down, raycastDistance, groundLayer);
 
         // If the ray hits something, the object is grounded.
         return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (raycastStartingPos == null) return;
+
+        Vector3 fromPos = new Vector3(transform.position.x - playerWidth / 2, raycastStartingPos.y, transform.position.z);
+        Vector3 toPos = new Vector3(transform.position.x + playerWidth / 2, raycastStartingPos.y, transform.position.z);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(fromPos, toPos);
     }
 }
